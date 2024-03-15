0419599622d8d88999fc29e5d1cd1f6b664ea0d9
MarkitosGood <163368980+MarkitosGood@users.noreply.github.com>
2024-03-14 18:53:10 -0600

    By Mark

---
+++ PortafolioByMark/Portafolio_By_Mark/script.js
@@ -0,0 +1,37 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("Configuración");
+        habilidades[1].classList.add("estetica");
+        habilidades[1].classList.add("creatividad");
+        habilidades[1].classList.add("experiencia");
+        habilidades[7].classL");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
